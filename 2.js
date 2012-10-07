a69a628a0c51b28f4b1eb88be899ad2a579488c0
joe.egan <joeegan@inbox.com>
2012-10-07 23:03:16 +0100

    js solution for #2

---
+++ 2.js
@@ -0,0 +1,19 @@
+function fibonacci(limit, arr, prevPrevTerm, prevTerm) {
+   var term = prevTerm + prevPrevTerm;
+   if (term <= limit) 
+      arr.push(term)
+   else 
+      return arr;
+   return fibonacci(limit, arr, prevTerm, term);
+}
+
+function sumEvens(arr, result) {
+   var result = result || 0;
+   if (arr[0] % 2 == 0) 
+      result += arr[0];
+   if (arr.length == 1) 
+      return result
+   return sumEvens(arr.slice(1), result);
+}
+
+console.log(sumEvens(fibonacci(4000000, [1, 2], 1, 2)));
